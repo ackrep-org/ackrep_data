5b233be3701d271ad5684dd07c5d035baf228b50
Robert Heedt <robert.heedt@gmail.com>
2020-07-11 12:08:51 +0200

    Add onion skinned animation

--- playground/acrobot_solution/solution.py
+++ playground/acrobot_solution/solution.py
@@ -2,6 +2,9 @@
 import sys
 from pytrajectory import TransitionProblem, log
 import numpy as np
+import sympy
+import symbtools as st
+import symbtools.visualisation as vt
 from sympy import cos, sin
 import os
 
@@ -85,8 +88,10 @@ def save_plot(problem_spec, solution_data):
     uu = np.array([solution_data.u_func(t)[0] for t in tt])
     xx = np.array([solution_data.x_func(t) for t in tt])
 
-    plt.figure(figsize=(5,5))
-    ax1 = plt.subplot(211)
+    result_dict = dict(tt=tt, uu=uu, xx=xx)
+    
+    fig = plt.figure(figsize=(5,8))
+    ax1 = plt.subplot(311)
     plt.plot(tt, xx[:, 0], label=r"$\theta_2$ [rad]")
     plt.plot(tt, xx[:, 1], label=r"$\dot \theta_2$ [rad/s]")
     plt.plot(tt, xx[:, 2], label=r"$\theta_1$ [rad]")
@@ -95,13 +100,35 @@ def save_plot(problem_spec, solution_data):
     plt.legend()
     plt.ylabel('state')
 
-    plt.subplot(212, sharex=ax1)
+    plt.subplot(312, sharex=ax1)
     plt.plot(tt, uu)
     plt.ylabel(r"input | $\ddot \theta_2$ [rad/s²]")
     plt.xlabel('$t$ [s]')
 
+    # onion skinned animation
+    l = 0.5  # visual linkage length
+
+    ttheta = st.symb_vector("theta1:3")
+    p0 = sympy.Matrix([0, 0])
+    p1 = p0 + l * sympy.Matrix([cos(ttheta[0]), sin(ttheta[0])])
+    p2 = p1 + l * sympy.Matrix([cos(ttheta[0] + ttheta[1]), sin(ttheta[0] + ttheta[1])])
+
+    vis = vt.Visualiser(ttheta, xlim=(-1.2, 1.2), ylim=(-1.2, 1.2), aspect='equal')
+    vis.add_linkage([p0, p1, p2], color='black')
+
+    _, ax = vis.create_default_axes(fig=fig, add_subplot_args=313)
+    plt.sca(ax)
+    plt.axis('off')
+    ax.grid(False)
+
+    frames = [0, 180, 300, 440, 600, 720, 850, 999]
+    frame_data = result_dict['xx'][:, (2, 0)]
+
+    vis.plot_onion_skinned(frame_data[frames, :], axes=ax)
+
     plt.tight_layout()
 
+    # save image
     sol_dir = os.path.join(os.path.dirname(__file__), '_solution_data')
 
     if not os.path.isdir(sol_dir):
