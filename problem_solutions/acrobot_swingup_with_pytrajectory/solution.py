f1dc8e5818e2a0c548b2cbe1e642bc87061f07df
Robert Heedt <robert.heedt@gmail.com>
2020-10-01 13:01:31 +0200

    Reuse state function for both spec and solution

--- problem_solutions/acrobot_swingup_with_pytrajectory/solution.py
+++ problem_solutions/acrobot_swingup_with_pytrajectory/solution.py
@@ -16,41 +16,6 @@ class SolutionData():
     pass
 
 
-def f(xx, uu, uuref, t, pp):
-    """ Right hand side of the vectorfield defining the system dynamics
-    :param xx:       state
-    :param uu:       input
-    :param uuref:    reference input (not used)
-    :param t:        time (not used)
-    :param pp:       additionial free parameters  (not used)
-    :return:        xdot
-    """
-    x1, x2, x3, x4 = xx
-    u1, = uu
-    
-    m = 1.0             # masses of the rods [m1 = m2 = m]
-    l = 0.5             # lengths of the rods [l1 = l2 = l]
-    
-    I = 1/3.0*m*l**2    # moments of inertia [I1 = I2 = I]
-    g = 9.81            # gravitational acceleration
-    
-    lc = l/2.0
-    
-    d11 = m*lc**2+m*(l**2+lc**2+2*l*lc*cos(x1))+2*I
-    h1 = -m*l*lc*sin(x1)*(x2*(x2+2*x4))
-    d12 = m*(lc**2+l*lc*cos(x1))+I
-    phi1 = (m*lc+m*l)*g*cos(x3)+m*lc*g*cos(x1+x3)
-
-    ff = np.array([     x2,
-                        u1,
-                        x4,
-                -1/d11*(h1+phi1+d12*u1)
-                ])
-    
-    return ff
-
-
-
 def solve(problem_spec):
     # system state boundary values for a = 0.0 [s] and b = 2.0 [s]
     xa = problem_spec.xx_start
@@ -63,11 +28,23 @@ def solve(problem_spec):
 
     ua = problem_spec.u_start
     ub = problem_spec.u_end
+
+    def f_pytrajectory(xx, uu, uuref, t, pp):
+        """ Right hand side of the vectorfield defining the system dynamics
+        :param xx:       state
+        :param uu:       input
+        :param uuref:    reference input (not used)
+        :param t:        time (not used)
+        :param pp:       additionial free parameters  (not used)
+        :return:        xdot
+        """
+
+        return problem_spec.rhs(xx, uu)
     
     first_guess = problem_spec.first_guess
 
     # create the trajectory object
-    S = TransitionProblem(f, a=0.0, b=T_end, xa=xa, xb=xb, ua=ua, ub=ub, constraints=con, use_chains=True, first_guess=first_guess)
+    S = TransitionProblem(f_pytrajectory, a=0.0, b=T_end, xa=xa, xb=xb, ua=ua, ub=ub, constraints=con, use_chains=True, first_guess=first_guess)
 
     # alter some method parameters to increase performance
     S.set_param('su', 10)
