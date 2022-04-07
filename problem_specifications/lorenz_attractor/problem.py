1fb9cdf9d153e7bdebe45b729bd7feda585f09c3
Julius <ichderjulius@gmx.de>
2022-04-07 13:07:08 +0200

    feat: playing around with importing models directly feat: added .vscode to gitignore

--- problem_specifications/lorenz_attractor/problem.py
+++ problem_specifications/lorenz_attractor/problem.py
@@ -8,7 +8,9 @@ import sympy as sp
 from sympy import cos, sin
 from math import pi
 from ackrep_core import ResultContainer
-
+import sys, os
+from ipydex import IPS, activate_ips_on_exception  # for debugging only
+import pdb
 
 class ProblemSpecification(object):
     # system symbols for setting up the ODEs
@@ -20,32 +22,43 @@ class ProblemSpecification(object):
     xx0 = np.array([0.1, 0.1, 0.1])  # initial condition
 
     tt = np.linspace(0, 30, 10000) # vector of times for simulation
-
-
-    @staticmethod
-    def rhs(t, xx):
-        """ Right hand side of the ODEs
-        :param xx:   system states
-        :return:     nonlinear state space
-        """
-        r = 35  
-        b = 2  
-        sigma = 20
+    parentpath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir,os.pardir,os.pardir))
+    parameter_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir,os.pardir,os.pardir,"_02_Implementations","Lorenz_Attractor"))
+    print(parentpath)
+    sys.path.insert(1, parentpath)
+    sys.path.insert(1, parameter_path)
+    # activate_ips_on_exception()
+    breakpoint()
+    import Catalog_of_dynamical_and_control_system_models._02_Implementations.Lorenz_Attractor.Lorenz_Attractor_class as ModelClass
+    
+    model = ModelClass.Model()
+    
+    # rhs_xx_pp_symb = model.get_rhs_symbolic()
+    rhs = model.get_rhs_func()
+    # @staticmethod
+    # def rhs(t, xx):
+    #     """ Right hand side of the ODEs
+    #     :param xx:   system states
+    #     :return:     nonlinear state space
+    #     """
+    #     r = 35  
+    #     b = 2  
+    #     sigma = 20
      
 
-        x, y, z = xx
+    #     x, y, z = xx
 
-        # motion of equations
-        x_dot = - sigma*x + sigma*y
-        y_dot = -x*z + r*x - y
-        z_dot = x*y - b*z
+    #     # motion of equations
+    #     x_dot = - sigma*x + sigma*y
+    #     y_dot = -x*z + r*x - y
+    #     z_dot = x*y - b*z
 
 
-        ff = np.array([x_dot,
-                        y_dot,
-                        z_dot])
+    #     ff = np.array([x_dot,
+    #                     y_dot,
+    #                     z_dot])
 
-        return ff
+    #     return ff
 
 def evaluate_solution(solution_data):
     """
