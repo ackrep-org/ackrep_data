6a44b49b6315e954a3e4d4d5e4bb713f184b37ed
Julius <ichderjulius@gmx.de>
2022-04-07 13:58:50 +0200

    feat: import of model rhs successfull. important change in e.g. Lorenz_Attractor_class.py: from . import lorenz_parameter , params = lorenz_parameter instead of using importlib

--- problem_specifications/lorenz_attractor/problem.py
+++ problem_specifications/lorenz_attractor/problem.py
@@ -22,18 +22,16 @@ class ProblemSpecification(object):
     xx0 = np.array([0.1, 0.1, 0.1])  # initial condition
 
     tt = np.linspace(0, 30, 10000) # vector of times for simulation
+    
+    
     parentpath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir,os.pardir,os.pardir))
-    parameter_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir,os.pardir,os.pardir,"_02_Implementations","Lorenz_Attractor"))
     print(parentpath)
     sys.path.insert(1, parentpath)
-    sys.path.insert(1, parameter_path)
-    # activate_ips_on_exception()
-    breakpoint()
+
     import Catalog_of_dynamical_and_control_system_models._02_Implementations.Lorenz_Attractor.Lorenz_Attractor_class as ModelClass
-    
+
     model = ModelClass.Model()
-    
-    # rhs_xx_pp_symb = model.get_rhs_symbolic()
+
     rhs = model.get_rhs_func()
     # @staticmethod
     # def rhs(t, xx):
