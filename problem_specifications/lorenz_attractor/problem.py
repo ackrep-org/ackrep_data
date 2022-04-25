3eb965bf696786473c6631138aad0056dcdf10fb
RST98\Julius Fiedler <ichderjulius@gmx.de>
2022-04-25 15:03:29 +0200

    fix: cleanup of old comments

--- problem_specifications/lorenz_attractor/problem.py
+++ problem_specifications/lorenz_attractor/problem.py
@@ -10,66 +10,15 @@ from math import pi
 from ackrep_core import ResultContainer
 import sys, os
 from ipydex import IPS, activate_ips_on_exception  # for debugging only
-import pdb
+from system_models.lorenz_system.system_model import Model 
 
 class ProblemSpecification(object):
-    # system symbols for setting up the ODEs
-    # x, y, z = sp.symbols("x, y, z")
-  
-    
-    
-    # xx = sp.Matrix([x, y, z])  # states of system
     xx0 = np.array([0.1, 0.1, 0.1])  # initial condition
-
     tt = np.linspace(0, 30, 10000) # vector of times for simulation
     
-    ### ----- Version 3: internal import ----- ###
-    from system_models.lorenz_system.system_model import Model 
     model = Model()
     rhs = model.get_rhs_func()
 
-    ### ----- Version 2: external import ----- ###
-#     parentpath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir,os.pardir,os.pardir))
-#     # print(parentpath)
-#     sys.path.insert(1, parentpath)
-
-#     import Catalog_of_dynamical_and_control_system_models._02_Implementations.Lorenz_Attractor.Lorenz_Attractor_class as ModelClass
-# #
-#     model = ModelClass.Model()
-#     rhs_symbolic = model.get_rhs_symbolic()
-#     rhs = model.get_rhs_func()
-#     print("Copy-pastable code equations:\n")
-#     for i, eq in enumerate(rhs_symbolic):
-#         print(f"dot_x{i}=", eq)
-#     # M:\Uni\SHK_Wissensrep\Catalog_of_dynamical_and_control_system_models\_01_Models\Lorenz_Attractor\Lorenz_Attractor_Documentation.pdf
-#     pdf_path = os.path.join(parentpath, "Catalog_of_dynamical_and_control_system_models", "_01_Models", "Lorenz_Attractor","Lorenz_Attractor_Documentation.pdf")
-    
-
-    ### ----- Version 1: hardcoded ----- ###
-    # @staticmethod
-    # def rhs(t, xx):
-    #     """ Right hand side of the ODEs
-    #     :param xx:   system states
-    #     :return:     nonlinear state space
-    #     """
-    #     r = 35  
-    #     b = 2  
-    #     sigma = 20
-     
-
-    #     x, y, z = xx
-
-    #     # motion of equations
-    #     x_dot = - sigma*x + sigma*y
-    #     y_dot = -x*z + r*x - y
-    #     z_dot = x*y - b*z
-
-
-    #     ff = np.array([x_dot,
-    #                     y_dot,
-    #                     z_dot])
-
-    #     return ff
 
 def evaluate_solution(solution_data):
     """
