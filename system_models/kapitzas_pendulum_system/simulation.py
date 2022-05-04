fix: update to simulation.py to show numerical error

system_models/kapitzas_pendulum_system/simulation.py
@@ -80,6 +80,8 @@ def evaluate_simulation(simulation_data):
     
     target_states = [3.1206479177994066, 0.22532090507016805]
 
-    success = all(abs(simulation_data.y[i][-1] - target_states[i]) < 1e-2 for i in np.arange(0, len(simulation_data.y)))
+    rc = ResultContainer(score=1.0)
+    rc.target_state_errors = [simulation_data.y[i][-1] - target_states[i] for i in np.arange(0, len(simulation_data.y))]
+    rc.success = all(abs(np.array(rc.target_state_errors)) < 1e-2)
     
-    return ResultContainer(success=success, score=1.0)
+    return rc