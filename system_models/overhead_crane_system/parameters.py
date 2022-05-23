54519c47dd3803283e0c188bec83f6c862ad8de4
lxchtschexn <74259508+lxchtschexn@users.noreply.github.com>
2022-05-23 17:09:38 +0200

    overhead crane not finished
    not ready, not working properly

---
+++ system_models/overhead_crane_system/parameters.py
@@ -0,0 +1,79 @@
+
+import sys
+import os
+import numpy as np
+import sympy as sp
+
+import tabulate as tab
+
+
+#link to documentation with examples: 
+#
+
+
+# set model name
+model_name = "overhead crane"
+
+
+# ---------- create symbolic parameters
+pp_symb = [m, M, l, g] = sp.symbols("m, M, l, g", real=True)
+
+
+
+
+# ---------- create symbolic parameter functions
+# parameter values can be constant/fixed values OR set in relation to other parameters (for example: a = 2*b)  
+m_sf = 20
+M_sf = 100
+l_sf = 3
+g_sf = 9.81
+
+# list of symbolic parameter functions
+# trailing "_sf" stands for "symbolic parameter function"
+pp_sf = [m_sf, M_sf, l_sf, g_sf]
+
+
+#  ---------- list for substitution
+# -- entries are tuples like: (independent symbolic parameter, numerical value)
+pp_subs_list = []
+
+
+# OPTONAL: Dictionary which defines how certain variables shall be written
+# in the table - key: Symbolic Variable, Value: LaTeX Representation/Code
+# useful for example for complex variables: {Z: r"\underline{Z}"}
+latex_names = {}
+
+
+# ---------- Define LaTeX table
+
+# Define table header 
+# DON'T CHANGE FOLLOWING ENTRIES: "Symbol", "Value"
+tabular_header = ["Parameter Name", "Symbol", "Value", "Unit"]
+
+# Define column text alignments
+col_alignment = ["left", "center", "left", "left"]
+
+
+# Define Entries of all columns before the Symbol-Column
+# --- Entries need to be latex code
+col_1 = ["mass of the last"
+         "mass of the wagon",
+         "rope length", 
+         "acceleration due to gravitation",] 
+
+# contains all lists of the columns before the "Symbol" Column
+# --- Empty list, if there are no columns before the "Symbol" Column
+start_columns_list = [col_1]
+
+
+# Define Entries of the columns after the Value-Column
+# --- Entries need to be latex code
+col_4 = ["kg",
+         "kg",
+         "m",
+         r"$\frac{m}{s^2}$"
+         ]
+
+# contains all lists of columns after the FIX ENTRIES
+# --- Empty list, if there are no columns after the "Value" column
+end_columns_list = [col_4]
