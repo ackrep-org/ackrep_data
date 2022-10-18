b01ec6700ae637b9515a0553b3c18f268e0ab7d7
JuliusFiedler <ichderjulius@gmx.de>
2022-10-18 14:25:25 +0200

    update four bar linkage

--- system_models/four_bar_linkage_system/parameters.py
+++ system_models/four_bar_linkage_system/parameters.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import numpy as np
@@ -6,7 +7,7 @@ import sympy as sp
 import tabulate as tab
 
 
-# link to documentation with examples: https://ackrep-doc.readthedocs.io/en/latest/devdoc/contributing_data.html
+#link to documentation with examples: https://ackrep-doc.readthedocs.io/en/latest/devdoc/contributing_data.html
 
 
 # set model name
@@ -14,30 +15,29 @@ model_name = "four-bar linkage"
 
 
 # ---------- create symbolic parameters
-pp_symb = [s1, s2, s3, m1, m2, m3, J1, J2, J3, l1, l2, l3, l4, g] = sp.symbols(
-    "s1, s2, s3, m1, m2, m3, J1, J2, J3, l1, l2, l3, l4, g", real=True
-)
+pp_symb = [s1, s2, s3, m1, m2, m3, J1, J2, J3, l1, l2, l3, l4, g] = sp.symbols("s1, s2, s3, m1, m2, m3, J1, J2, J3, l1, l2, l3, l4, g", real=True)
+
 
 
 # ---------- create symbolic parameter functions
-# parameter values can be constant/fixed values OR set in relation to other parameters (for example: a = 2*b)
-s1_sf = 1 / 2
-s2_sf = 1 / 2
-s3_sf = 1 / 2
-m1_sf = 1
-m2_sf = 1
-m3_sf = 3
-J1_sf = 1 / 12
-J2_sf = 1 / 12
-J3_sf = 1 / 12
-l1_sf = 0.8
-l2_sf = 1.5
-l3_sf = 1.5
-l4_sf = 2
-g_sf = 9.81
+# parameter values can be constant/fixed values OR set in relation to other parameters (for example: a = 2*b)  
+s1_sf=1/2
+s2_sf=1/2
+s3_sf=1/2
+m1_sf=1
+m2_sf=1
+m3_sf=3
+J1_sf=1/12
+J2_sf=1/12
+J3_sf=1/12
+l1_sf=0.8
+l2_sf=1.5
+l3_sf=1.5
+l4_sf=2
+g_sf=9.81
 
 # list of symbolic parameter functions
-# tailing "_sf" stands for "symbolic parameter function"
+# trailing "_sf" stands for "symbolic parameter function"
 pp_sf = [s1_sf, s2_sf, s3_sf, m1_sf, m2_sf, m3_sf, J1_sf, J2_sf, J3_sf, l1_sf, l2_sf, l3_sf, l4_sf, g_sf]
 
 
@@ -54,7 +54,7 @@ latex_names = {}
 
 # ---------- Define LaTeX table
 
-# Define table header
+# Define table header 
 # DON'T CHANGE FOLLOWING ENTRIES: "Symbol", "Value"
 tabular_header = ["Parameter Name", "Symbol", "Value", "Unit"]
 
@@ -64,22 +64,21 @@ col_alignment = ["left", "center", "left", "center"]
 
 # Define Entries of all columns before the Symbol-Column
 # --- Entries need to be latex code
-col_1 = [
-    "center of gravity distance of first bar",
-    "center of gravity distance of second bar",
-    "center of gravity distance of third bar",
-    "mass of first bar",
-    "mass of second bar",
-    "mass of third bar",
-    "moment of inertia",
-    "moment of inertia",
-    "moment of inertia",
-    "length of first bar",
-    "length of second bar",
-    "length of third bar",
-    "length of fourth bar",
-    "acceleration due to gravity",
-]
+col_1 = ["distance from the joint to the center of gravity of link 1",
+         "distance from the joint to the center of gravity of link 2",
+         "distance from the joint to the center of gravity of link 3",
+         "mass of link 1",
+         "mass of link 2",
+         "mass of link 3",
+         "moment of inertia of link 1",
+         "moment of inertia of link 2",
+         "moment of inertia of link 3",
+         "length of link 1",
+         "length of link 2",
+         "length of link 3",
+         "length of link 4",
+         "acceleration due to gravity"
+         ] 
 
 # contains all lists of the columns before the "Symbol" Column
 # --- Empty list, if there are no columns before the "Symbol" Column
@@ -88,22 +87,21 @@ start_columns_list = [col_1]
 
 # Define Entries of the columns after the Value-Column
 # --- Entries need to be latex code
-col_4 = [
-    "m",
-    "m",
-    "m",
-    "kg",
-    "kg",
-    "kg",
-    r"$\frac{kg}{m^2}$",
-    r"$\frac{kg}{m^2}$",
-    r"$\frac{kg}{m^2}$",
-    "m",
-    "m",
-    "m",
-    "m",
-    r"$\frac{m}{s^2}$",
-]
+col_4 = ["m",
+         "m",
+         "m",
+         "kg",
+         "kg",
+         "kg",
+         r"$\frac{kg}{m^2}$",
+         r"$\frac{kg}{m^2}$",
+         r"$\frac{kg}{m^2}$",
+         "m",
+         "m",
+         "m",
+         "m",
+         r"$\frac{m}{s^2}$"
+         ]
 
 # contains all lists of columns after the FIX ENTRIES
 # --- Empty list, if there are no columns after the "Value" column
