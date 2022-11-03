7620b9d3b124e62544b9c0c75a39c85c99ee9185
lxchtschexn <74259508+lxchtschexn@users.noreply.github.com>
2022-11-03 13:22:31 +0100

    new model ball beam

---
+++ system_models/ball_beam_system/parameters.py
@@ -0,0 +1,81 @@
+
+import sys
+import os
+import numpy as np
+import sympy as sp
+
+import tabulate as tab
+
+
+#link to documentation with examples: https://ackrep-doc.readthedocs.io/en/latest/devdoc/contributing_data.html
+
+
+# set model name
+model_name = "ball beam"
+
+
+# ---------- create symbolic parameters
+pp_symb = [m1, J1, J2, r, g] = sp.symbols("m1, J1, J2, r, g", real=True)
+
+# ---------- create symbolic parameter functions
+# parameter values can be constant/fixed values OR set in relation to other parameters (for example: a = 2*b)  
+m1_sf = 0.05
+J1_sf = 0.02
+J2_sf = 2e-6
+r_sf = 0.01
+g_sf = 10
+
+# list of symbolic parameter functions
+# tailing "_sf" stands for "symbolic parameter function"
+pp_sf = [m1_sf, J1_sf, J2_sf, r_sf, g_sf]
+
+
+#  ---------- list for substitution
+# -- entries are tuples like: (independent symbolic parameter, numerical value)
+pp_subs_list = []
+
+
+# OPTONAL: Dictionary which defines how certain variables shall be written
+# in the table - key: Symbolic Variable, Value: LaTeX Representation/Code
+# useful for example for complex variables: {Z: r"\underline{Z}"}
+latex_names = {}
+
+
+# ---------- Define LaTeX table
+
+# Define table header 
+# DON'T CHANGE FOLLOWING ENTRIES: "Symbol", "Value"
+tabular_header = ["Parameter Name", "Symbol", "Value", "Unit"]
+
+# Define column text alignments
+col_alignment = ["left", "center", "left", "center"]
+
+
+# Define Entries of all columns before the Symbol-Column
+# --- Entries need to be latex code
+col_1 = [
+        "mass of the ball",
+        "moment of inertia of the beam",
+        "moment of inertia of the ball",
+        "radius of the ball",
+        "acceleration due to gravity"
+        ] 
+
+# contains all lists of the columns before the "Symbol" Column
+# --- Empty list, if there are no columns before the "Symbol" Column
+start_columns_list = [col_1]
+
+
+# Define Entries of the columns after the Value-Column
+# --- Entries need to be latex code
+col_4 = [
+        "kg",
+        r"$kg \cdot m^2$",
+        r"$kg \cdot m^2$",
+        "m",
+        r"$\frac{m}{s^2}$"
+        ]
+
+# contains all lists of columns after the FIX ENTRIES
+# --- Empty list, if there are no columns after the "Value" column
+end_columns_list = [col_4]
---
+++ system_models/ball_beam_system/system_model.py
@@ -0,0 +1,123 @@
+
+import sympy as sp
+import symbtools as st
+from symbtools import modeltools as mt
+import importlib
+import sys, os
+#from ipydex import IPS, activate_ips_on_exception  
+
+from ackrep_core.system_model_management import GenericModel, import_parameters
+
+# Import parameter_file
+params = import_parameters()
+
+
+#link to documentation with examples: https://ackrep-doc.readthedocs.io/en/latest/devdoc/contributing_data.html
+
+
+class Model(GenericModel): 
+
+    def initialize(self):
+        """
+        this function is called by the constructor of GenericModel
+
+        :return: None
+        """
+        
+        # ---------start of edit section--------------------------------------
+        # Define number of inputs -- MODEL DEPENDENT
+        self.u_dim = 2
+
+        # Set "sys_dim" to constant value, if system dimension is constant 
+        self.sys_dim = 4
+
+        # ---------end of edit section----------------------------------------
+
+        # check existence of params file
+        self.has_params = True
+        self.params = params
+        
+
+    # ----------- SET DEFAULT INPUT FUNCTION ---------- # 
+    # --------------- Only for non-autonomous Systems
+    def uu_default_func(self):
+        """
+        define input function
+    
+        :return:(function with 2 args - t, xx_nv) default input function 
+        """ 
+        
+        # ---------start of edit section--------------------------------------
+        def uu_rhs(t, xx_nv):
+            """
+            sequence of numerical input values
+
+            :param t:(scalar or vector) time
+            :param xx_nv:(vector or array of vectors) numeric state vector
+            :return:(list) numeric inputs 
+            """ 
+            if t < 1: 
+                u_num_2 = 0
+            elif 1 < t < 2:
+                u_num_2 = 2
+            else: 
+                u_num_2 = 0
+            u_num_1 = 0
+            return [u_num_1, u_num_2]
+        # ---------end of edit section----------------------------------------
+
+        return uu_rhs
+
+
+    # ----------- SYMBOLIC RHS FUNCTION ---------- # 
+
+    def get_rhs_symbolic(self):
+        """
+        define symbolic rhs function
+
+        :return: matrix of symbolic rhs-functions
+        """
+        if self.dxx_dt_symb is not None:
+            return self.dxx_dt_symb
+
+
+        # ---------start of edit section--------------------------------------
+        x1, x2, x3, x4 = self.xx_symb   #state components
+        m1, J1, J2, r, g = self.pp_symb   #parameters
+    
+        u1, u2 = self.uu_symb   # inputs
+
+        q = sp.Matrix([[x1], [x2]])
+        xdot1, xdot2 = sp.symbols('xdot1, xdot2')
+
+        # Geometrie: (Einheitsvektoren)>>>Spezial
+        ex = sp.Matrix([1,0])
+        ey = sp.Matrix([0,1])
+
+        # Koordinaten der Schwerpunkte der zwei Koerper
+        S1 = 0
+        S2 = x1*mt.Rz(x2)*ex
+
+        # Geschwindigkeiten der Koerper
+        Sd2 = st.time_deriv(S2, q)
+
+        # Kinetische Energie
+        T_trans = (m1*Sd2.T*Sd2) /2 # Translationsenergie
+        T_rot = (J1*xdot2**2+J2*xdot2**2+J2*(xdot1/r)**2)/2 # Rotationsenergie
+        T = T_trans[0] + T_rot #Kinetische Energie
+
+        # Potentielle Energie
+        V = m1*g*S2[1]
+
+        external_forces = sp.Matrix([[0, u2]])
+
+        mod = mt.generate_symbolic_model(T, V, q, external_forces)
+        
+        mod.calc_state_eq(simplify=False)
+
+        state_eq = mod.state_eq.subs([(xdot1, x3), (xdot2, x4)])
+        # ---------end of edit section----------------------------------------
+
+
+        return state_eq
+    
